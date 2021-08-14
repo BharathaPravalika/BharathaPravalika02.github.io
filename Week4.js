16fd588167d55dea6f88f4c2629bd0f2d5107312
BharathaPravalika <83343601+BharathaPravalika@users.noreply.github.com>
2021-08-14 16:59:45 +0530

    Update Week4.js

--- Week4.js
+++ Week4.js
@@ -1,13 +1,14 @@
 var names = new Array();
-names[0]="Bharatha";
-names[1]="Jasmine";
-names[2]="Sony";
-names[3]="Pravalika";
-names[4]="john";
-names[5]="Master";
-names[6]="jason";
-names[7]="Flag";
-names[8]="Jimmy";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="Jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Jim";
 
 for(var i=0;i<names.length;i++){
 	if(names[i].charAt(0)=='j' || names[i].charAt(0) == 'J'){
